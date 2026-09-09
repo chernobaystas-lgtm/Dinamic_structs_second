9364be718fa9f61fc2be0d33e078df1022082cce
Nrigot <chernobaystas@gmail.com>
2026-09-09 21:04:38 +0300

    First exersice

--- Book_with_dinamic_struct.cpp
+++ Book_with_dinamic_struct.cpp
@@ -54,11 +54,54 @@ private:
 
 };
 
+
+
+class Toy : public Product {
+
+public:
+	Toy(int a, string m) : ageLimit{ a }, manufacturer{ m } {
+		cout << "Toy constructor called" << endl;
+	}
+	Toy(int a, string m, string Toyname) : ageLimit{ a }, manufacturer{ m }{
+		name = Toyname;
+		cout << "Toy constructor called" << endl;
+	}
+	Toy(string nameToy, string categoryName, double price, int ageLimit, string manufacturer) : Product(nameToy, categoryName, price), ageLimit(ageLimit), manufacturer(manufacturer) {
+	}
+
+	~Toy() {
+		cout << "Toy destructor called" << endl;
+	}
+
+	void setprice(double price) {
+		if(ageLimit > 5 && this ->price-price >10) {
+			this->price = price;
+		}
+	}
+
+	string productTostring() const {
+		return "Product Name: " + name + ", Category: " + getCategoryName() + ", Price: " + to_string(price) + ", Age Limit: " + to_string(ageLimit) + ", Manufacturer: " + manufacturer;
+	}
+
+	void show() const {
+		cout << productTostring() << endl;
+	}
+
+private:
+	string manufacturer{ "" };
+	int ageLimit{ 0 };
+
+};
+
+
 int main()
 {
 	SetConsoleOutputCP(CP_UTF8);
 	SetConsoleCP(CP_UTF8);
 
-
+	Product product1("Laptop", "Electronics", 999.99);
+	product1.show();
+	Toy toy1(3, "Hasbro", "Action Figure");
+	toy1.show();
 	return 0;
 }
