b53299888dcd0aca57793345be237461a9e9f2ed
Nrigot <chernobaystas@gmail.com>
2026-09-14 21:26:56 +0300

    Final

--- Book_with_dinamic_struct.cpp
+++ Book_with_dinamic_struct.cpp
@@ -5,63 +5,171 @@
 
 using namespace std;
 
-
-struct Date {
-    int year;
-    int month;
-    int day;
-    int hour;
-    int minuts;
-    int seconds;
-
-    Date() : year(0), month(0), day(0), hour(0), minuts(0), seconds(0) {}
-
-    void input() {
-        cout << "Enter year: ";
-        cin >> year;
-        cout << "Enter month: ";
-        cin >> month;
-        cout << "Enter day: ";
-        cin >> day;
-        cout << "Enter hour: ";
-        cin >> hour;
-        cout << "Enter minutes: ";
-        cin >> minuts;
-        cout << "Enter seconds: ";
-        cin >> seconds;
-    }
-
-    void show() const {
-        cout << day << "." << month << "." << year << " "
-            << hour << ":" << minuts << ":" << seconds << endl;
-    }
-};
-
-struct Name {
+struct Name
+{
     string name;
     string second_name;
     string lastname;
 
+    Name()
+        : name(""), second_name(""), lastname("")
+    {}
 
-    Name() : name(""), second_name(""), lastname("") {}
-    Name(const string& fullName) : name(fullName), second_name(""), lastname("") {}
+    Name(const string& fullName)
+        : name(fullName), second_name(""), lastname("")
+    {}
 
+    Name(string name, string second_name, string lastname)
+        : name(name), second_name(second_name), lastname(lastname)
+    {}
 
-    void input() {
-        cout << "Enter name: " << endl;
-        cin >> name;
-        cout << "Enter second name: " << endl;
-        cin >> second_name;
-        cout << "Enter last name: " << endl;
-        cin >> lastname;
-    }
-
-    void show() const {
-        cout << "Name: " << name << " Second name: " << second_name
-            << " Last name: " << lastname << endl;
+    void show() const
+    {
+        cout << name << " "
+            << second_name << " "
+            << lastname << endl;
     }
 };
 
+
+class Entity
+{
+protected:
+    Name name;
+
+public:
+
+    Entity()
+        : name("No name")
+    {}
+
+    Entity(Name name)
+        : name(name)
+    {}
+
+    virtual ~Entity() = default;
+
+    virtual void SayName() const
+    {
+        cout << "Name: ";
+        name.show();
+    }
+};
+
+
+class Warrior : virtual public Entity
+{
+protected:
+    int stamina;
+
+public:
+
+    Warrior()
+        : Entity(), stamina(100)
+    {}
+
+    Warrior(Name name, int stamina)
+        : Entity(name), stamina(stamina)
+    {}
+
+    virtual ~Warrior() = default;
+
+    virtual void UseSword()
+    {
+        if (stamina >= 20)
+        {
+            cout << format(
+                "{} uses sword! Stamina: {} -> {}",
+                name.name,
+                stamina,
+                stamina - 20
+            ) << endl;
+
+            stamina -= 20;
+        }
+        else
+        {
+            cout << "Not enough stamina!" << endl;
+        }
+    }
+};
+
+
+class Mage : virtual public Entity
+{
+protected:
+    int mana;
+
+public:
+
+    Mage()
+        : Entity(), mana(100)
+    {}
+
+    Mage(Name name, int mana)
+        : Entity(name), mana(mana)
+    {}
+
+    virtual ~Mage() = default;
+
+    virtual void CastSpell()
+    {
+        if (mana >= 30)
+        {
+            cout << format(
+                "{} casts a spell! Mana: {} -> {}",
+                name.name,
+                mana,
+                mana - 30
+            ) << endl;
+
+            mana -= 30;
+        }
+        else
+        {
+            cout << "Not enough mana!" << endl;
+        }
+    }
+};
+
+
+class Spellblade : public Warrior, public Mage
+{
+public:
+
+    Spellblade()
+        : Entity(), Warrior(), Mage()
+    {}
+
+    Spellblade(Name name, int stamina, int mana)
+        : Entity(name),
+        Warrior(name, stamina),
+        Mage(name, mana)
+    {}
+
+    ~Spellblade() override = default;
+
+
+    void SayName() const override
+    {
+        cout << "Spellblade: ";
+        name.show();
+    }
+
+
+    void UseSword() override
+    {
+        cout << "[Spellblade] ";
+        Warrior::UseSword();
+    }
+
+
+    void CastSpell() override
+    {
+        cout << "[Spellblade] ";
+        Mage::CastSpell();
+    }
+};
 
 
 
@@ -69,6 +177,44 @@ int main()
 {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+
+
+    Spellblade warriorMage1(
+        Name("Arthur", "The", "Brave"),
+        100,
+        100
+    );
+
+    Spellblade warriorMage2(
+        Name("Merlin", "The", "Great"),
+        80,
+        120
+    );
+
+
+    cout << "===== FIRST SPELLBLADE =====" << endl;
+
+    warriorMage1.SayName();
+
+    warriorMage1.UseSword();
+    warriorMage1.CastSpell();
+
+
+    cout << "\n===== SECOND SPELLBLADE =====" << endl;
+
+    warriorMage2.SayName();
+
+    warriorMage2.UseSword();
+    warriorMage2.CastSpell();
+
+
+    cout << "\n===== BATTLE =====" << endl;
+
+    warriorMage1.UseSword();
+    warriorMage2.CastSpell();
+
+    warriorMage1.UseSword();
+    warriorMage2.CastSpell();
 
 
     return 0;
