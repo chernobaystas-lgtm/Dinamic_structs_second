e0fd74ab705ee8cdda5cb8359773321d80814ae6
Nrigot <chernobaystas@gmail.com>
2026-09-18 19:05:13 +0300

    End of tutorial

--- Book_with_dinamic_struct.cpp
+++ Book_with_dinamic_struct.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <windows.h>
 #include <cmath>
-
+#include <fstream>
 using namespace std;
 
 
@@ -12,6 +12,71 @@ int main()
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
+    char MyFile[] = "C:\\Users\\Admin\\Visual Studio\\OOP\\Book_with_dinamic_struct";
+
+    //ofstream Opened(MyFile);
+    //if (Opened.fail()) { //!Opened.is_open()
+    //    cerr << "Error" << endl;
+    //    system("pause");
+    //    return -1;
+    //}
+
+
+
+    //ofstream Opened;
+    //Opened.open(MyFile); //ios::binary|ios::app
+    fstream Opened;
+    Opened.open(MyFile);
+    if (Opened.fail()) { //!Opened.is_open()
+        cerr << "Error" << endl;
+        system("pause");
+        return -1;
+    }
+    string str{ "My Start" };
+
+    while (!Opened.eof()) {
+        getline(Opened, str);
+        cout << str << endl;
+    }
+ 
+    Opened.close();
+
+
+
+
+    fstream Opened(MyFile, ios::app);
+    Opened.open(MyFile);
+    if (Opened.fail()) { //!Opened.is_open()
+        cerr << "Error" << endl;
+        system("pause");
+        return -1;
+    }
+    string str{ "My New WAy" };
+    Opened << str;
+    Opened.close();
+
+
+
+
+    Opened.open(MyFile, ios::in);
+    if (Opened.fail()) { //!Opened.is_open()
+        cerr << "Error" << endl;
+        system("pause");
+        return -1;
+    }
+    string str{ "My Start" };
+
+    while (!Opened.eof()) {
+        getline(Opened, str);
+        cout << str << endl;
+    }
+
+    Opened.close();
+
+    /*string str{ "My Start" };
+    Opened << "Begin" << endl;
+    Opened << str;
+    Opened.close();*/
 
     return 0;
 }
