28b78ac3d49b33e9a83c202c6e80cd72940ab448
Nrigot <chernobaystas@gmail.com>
2026-09-23 19:53:49 +0300

    First exersice

--- Book_with_dinamic_struct.cpp
+++ Book_with_dinamic_struct.cpp
@@ -5,12 +5,39 @@
 #include <algorithm>
 using namespace std;
 
-
+void show(const std::vector<int>& list) {
+    for (int x : list) { cout << x << " "; }
+    cout << endl; 
+}
 
 int main()
 {
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
+
+
+
+    vector<int> numbers = { 11, 1, 56, -1, 2, 3, 4, 5, 999, 0 };
+
+    vector<int>::iterator;
+
+    
+
+    cout << numbers.back();
+    show(numbers);
+
+    sort(numbers.begin(), numbers.end());
+    show(numbers);
+
+    for_each(numbers.begin(), numbers.end(), [](int& x) { x += 1; });
+    show(numbers);
+
+    for_each(numbers.begin(), numbers.end(), [](int& x) { x -= 1; });
+    show(numbers);
+
+    int target = 2;
+    cout << erase(numbers, 5);
+    show(numbers);
 
 
     return 0;
