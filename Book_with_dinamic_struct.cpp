95ac245962fc72b6c7722184efbc9df2648538b4
Nrigot <chernobaystas@gmail.com>
2026-09-25 20:55:00 +0300

    Exersice 3

--- Book_with_dinamic_struct.cpp
+++ Book_with_dinamic_struct.cpp
@@ -1,120 +1,206 @@
 ﻿#include <iostream>
 #include <string>
-#include <windows.h>
-#include <vector>
-#include <set>
+#include <fstream>
+#include <sstream>
 #include <map>
-#include <iostream>
-#include <random>
-#include <fstream> 
-#include <algorithm>
+#include <cctype>
+#include <windows.h>
+
 using namespace std;
 
 
-
-enum class Answer { A, B, C, D, NONE };
-
-char answerToChar(Answer a) {
-    switch (a) {
-    case Answer::A: return 'A';
-    case Answer::B: return 'B';
-    case Answer::C: return 'C';
-    case Answer::D: return 'D';
-    default: return '?';
-    }
-}
-
-Answer charToAnswer(char c) {
-    switch (toupper(c)) {
-    case 'A': return Answer::A;
-    case 'B': return Answer::B;
-    case 'C': return Answer::C;
-    case 'D': return Answer::D;
-    default: return Answer::NONE;
-    }
-}
-
-struct Question {
-    string text;
-    string options[4];
+enum class Answer
+{
+    A,
+    B,
+    C,
+    D,
+    NONE
 };
 
-map<int, Answer> read_correct_answers(const string& filename) {
-    map<int, Answer> answers;
-    ifstream file(filename);
-    if (!file.is_open()) {
-        cerr << "Не удалось открыть файл з відповідями\n";
-        return answers;
+
+struct Question
+{
+    string text;          
+    string options[4];   
+    char correctAnswer;   
+};
+
+Answer charToAnswer(char c)
+{
+    c = toupper(c);
+
+    if (c == 'A')
+        return Answer::A;
+
+    if (c == 'B')
+        return Answer::B;
+
+    if (c == 'C')
+        return Answer::C;
+
+    if (c == 'D')
+        return Answer::D;
+
+    return Answer::NONE;
+}
+
+
+void LoadQuestions(map<int, Question>& questions)
+{
+    ifstream file("For_maps2.txt");
+
+    if (!file.is_open())
+    {
+        cout << "Oshibka: ne udalos otkryt For_maps2.txt!\n";
+        return;
     }
 
-    int num;
-    char letter;
-    while (file >> num >> letter) {
-        answers[num] = charToAnswer(letter);
+    string line;
+
+    while (getline(file, line))
+    {
+        if (line.empty())
+            continue;
+
+        if (line.size() >= 3 &&
+            (unsigned char)line[0] == 0xEF &&
+            (unsigned char)line[1] == 0xBB &&
+            (unsigned char)line[2] == 0xBF)
+        {
+            line.erase(0, 3);
+        }
+
+        stringstream ss(line);
+
+        string numberText;
+        string correctText;
+
+        Question question{};
+
+        getline(ss, numberText, '|');
+
+        getline(ss, question.text, '|');
+
+        for (int i = 0; i < 4; i++)
+        {
+            getline(ss, question.options[i], '|');
+        }
+
+        getline(ss, correctText, '|');
+
+        int number = stoi(numberText);
+
+        question.correctAnswer = toupper(correctText[0]);
+
+        questions[number] = question;
     }
 
     file.close();
-    return answers;
-};
+}
 
-int main() {
+
+int main()
+{
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    vector<Question> questions = {
-        {"Столиця України?", {"Львів", "Одеса", "Київ", "Харків"}},
-        {"Хто написав \"Кобзар\"?", {"Франко", "Шевченко", "Леся Українка", "Гоголь"}},
-        {"Найдовша річка України?", {"Дніпро", "Дністер", "Буг", "Десна"}},
-        {"Скільки областей в Україні?", {"24", "25", "26", "27"}},
-        {"У якому році Україна здобула незалежність?", {"1989", "1990", "1991", "1992"}},
-        {"Найвища гора України?", {"Говерла", "Петрос", "Піп Іван", "Ребра"}},
-        {"Яка мова офіційна в Україні?", {"Російська", "Українська", "Польська", "Англійська"}},
-        {"Автор \"Лісової пісні\"?", {"Шевченко", "Леся Українка", "Франко", "Коцюбинський"}},
-        {"Найбільше місто України за населенням?", {"Львів", "Одеса", "Харків", "Київ"}},
-        {"Валюта України?", {"Рубль", "Злотий", "Гривня", "Євро"}},
-        {"Скільки букв в українському алфавіті?", {"32", "33", "34", "35"}},
-        {"Національний символ України, крім прапора?", {"Тризуб", "Орел", "Лев", "Ведмідь"}}
-    };
+    map<int, Question> questions;
 
-    map<int, Answer> correct = read_correct_answers("For_maps2.txt");
-    if (correct.empty()) {
-        cerr << "Файл з відповідями порожній або не знайдений\n";
-        return 1;
+    LoadQuestions(questions);
+
+    if (questions.empty())
+    {
+        cout << "Voprosy ne zagruzheny.\n";
+        cout << "Proverte For_maps2.txt.\n";
+        system("pause");
+        return 0;
     }
+
+
+    cout << "====================================\n";
+    cout << "          VIKTORINA\n";
+    cout << "====================================\n";
+
+    cout << "Kolichestvo voprosov: "
+        << questions.size() << "\n";
+
 
     int score = 0;
-    const char letters[4] = { 'a', 'b', 'c', 'd' };
 
-    for (size_t i = 0; i < questions.size(); ++i) {
-        int num = static_cast<int>(i) + 1;
-        cout << num << ". " << questions[i].text << "\n";
-        for (int k = 0; k < 4; ++k) {
-            cout << "  " << letters[k] << ") " << questions[i].options[k] << "\n";
+
+    for (const auto& item : questions)
+    {
+        int number = item.first;
+
+        const Question& question = item.second;
+
+
+        cout << "\n";
+        cout << "------------------------------------\n";
+
+        cout << number << ". "
+            << question.text << "\n\n";
+
+
+        cout << "A) " << question.options[0] << "\n";
+        cout << "B) " << question.options[1] << "\n";
+        cout << "C) " << question.options[2] << "\n";
+        cout << "D) " << question.options[3] << "\n";
+
+
+        char answer;
+
+        while (true)
+        {
+            cout << "\nVash otvet: ";
+            cin >> answer;
+
+            answer = toupper(answer);
+
+
+            if (answer == 'A' ||
+                answer == 'B' ||
+                answer == 'C' ||
+                answer == 'D')
+            {
+                break;
+            }
+
+            cout << "Oshibka! Vvedite tolko A, B, C ili D.\n";
         }
-        cout << "Ваша відповідь: ";
 
-        char input;
-        cin >> input;
-        Answer userAnswer = charToAnswer(input);
-
-        if (correct.count(num) && userAnswer == correct[num]) {
+        
+        if (answer == question.correctAnswer)
+        {
+            cout << "Pravilno!\n";
             score++;
         }
+        else
+        {
+            cout << "Nepravilno!\n";
+        }
     }
 
-    cout << "==================================================\n";
-    cout << "Результат: " << score << " з " << questions.size() << " (12-бальна система)\n";
 
-    ofstream result("result.txt");
-    if (result.is_open()) {
-        result << "Результат тестування: " << score << " з " << questions.size() << "\n";
-        result.close();
-        cout << "Результат збережено у result.txt\n";
-    }
-    else {
-        cerr << "Не вдалося зберегти результат у файл\n";
-    }
+    cout << "\n====================================\n";
+    cout << "             REZULTAT\n";
+    cout << "====================================\n";
+
+    cout << "Pravilnyh otvetov: "
+        << score << " iz "
+        << questions.size() << "\n";
+
+
+    double percent =
+        static_cast<double>(score) /
+        questions.size() * 100.0;
+
+    cout << "Rezultat: "
+        << percent << "%\n";
+
+
+    system("pause");
 
     return 0;
-
-};
+}
